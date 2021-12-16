10cf5e3a49517f5152d325b26804a72067b9d9a0
Jason Yapri <jason@inergi.id>
2021-12-16 13:28:45 +0700

    Extracting Widgets (#20)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
+import 'package:hello_id/model/quote.dart';
+import 'package:hello_id/widget/quote_card.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList()
@@ -19,36 +20,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Oscar Wilde' , quote: 'The truth is rarely pure and never simple'),
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.quote,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600]
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              quote.author
-              ,
-              style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[800]
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +30,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
